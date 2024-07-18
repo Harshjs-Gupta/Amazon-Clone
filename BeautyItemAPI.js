a1851f0ab3505aac74c14c73fc4609d5f2a3a0aa
Harsh Gupta <harshgupta88911@gmail.com>
2024-07-18 13:48:56 +0530

    add an api which contain beauty products

---
+++ BeautyItemAPI.js
@@ -0,0 +1,5 @@
+// const beautyResponse = await fetch(
+//   "https://makeup-api.herokuapp.com/api/v1/products.json"
+// );
+// const makupData = await beautyResponse.json();
+// console.log(makupData);
